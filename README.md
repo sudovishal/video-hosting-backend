e1d010049a8577493688dccff1c565d62b330a32
sudovishal <shinde.vishal1809@gmail.com>
2024-03-08 20:46:32 +0530

    setup multer and cloudinary for file uploads

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Database is in another continent
+
+
+multer to local server to cloudinary -- industry production grade settings
+used multer and cloudinary for file uploading
+will be using advanced mongodb aggregation pipelines
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+// multer to local server to cloudinary -- industry production grade settings
+// multer to direct cloudinary also possible
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+// fs unlink()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded sucessfull
+    console.log(`File is uploaded on Cloudinary at: ${response.url}`);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
